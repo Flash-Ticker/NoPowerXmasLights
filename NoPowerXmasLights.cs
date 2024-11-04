84be056990d8d431b263588d85c4b50510dc520d
„flashticker“ <cooler-oliver@gmx.net>
2024-11-04 14:35:01 +0100

    no power abuse

--- NoPowerXmasLights.cs
+++ NoPowerXmasLights.cs
@@ -1,14 +1,18 @@
 using Oxide.Core;
 using Oxide.Core.Plugins;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("NoPowerXmasLights", "RustFlash", "1.0.0")]
+    [Info("NoPowerChristmasLights", "RustFlash", "1.0.0")]
     [Description("Allows you to use Christmas lights without a power connection")]
     public class NoPowerXmasLights : RustPlugin
     {
+        private readonly object False = false;
+        private readonly object Zero = 0;
+
         void OnServerInitialized()
         {
             UpdateAllLights(999999);
@@ -17,44 +21,51 @@ namespace Oxide.Plugins
         void OnEntitySpawned(AdvancedChristmasLights lightString)
         {
             if (lightString == null) return;
-            
+           
             NextTick(() => {
                 lightString.UpdateHasPower(999999, 1);
-                lightString.currentEnergy = 999999;
+                lightString.currentEnergy = 0;
                 lightString.SetFlag(BaseEntity.Flags.On, true);
                 lightString.SetFlag(BaseEntity.Flags.Reserved8, true);
-                lightString.SendNetworkUpdateImmediate();
             });
         }
 
         private void UpdateAllLights(int power)
         {
-            foreach (var lightString in UnityEngine.Object.FindObjectsOfType<AdvancedChristmasLights>())
+            foreach (var lightString in BaseNetworkable.serverEntities.OfType<AdvancedChristmasLights>())
             {
                 if (lightString != null)
                 {
                     lightString.UpdateHasPower(power, 1);
-                    lightString.currentEnergy = power;
-                    lightString.SetFlag(BaseEntity.Flags.On, power > 0);
-                    lightString.SetFlag(BaseEntity.Flags.Reserved8, power > 0);
-                    lightString.SendNetworkUpdateImmediate();
+                    lightString.currentEnergy = 0;
+                    lightString.SetFlag(BaseEntity.Flags.On, true);
+                    lightString.SetFlag(BaseEntity.Flags.Reserved8, true);
                 }
             }
         }
 
+        object OnConsumptionAmount(AdvancedChristmasLights lightString)
+        {
+            return Zero;
+        }
+
+        object OnRequiresPower(AdvancedChristmasLights lightString)
+        {
+            return False;
+        }
+
+        object OnMaximalPowerOutput(IOEntity entity)
+        {
+            if (entity is AdvancedChristmasLights)
+            {
+                return 0;
+            }
+            return null;
+        }
+        
         void Unload()
         {
             UpdateAllLights(0);
-        }
-
-        object OnConsumptionAmount(AdvancedChristmasLights lightString)
-        {
-            return 0;
-        }
-
-        object OnRequiresPower(AdvancedChristmasLights lightString) 
-        {
-            return false;
         }
     }
 }
